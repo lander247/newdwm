6f4d56de1ecdf6da9ef29ea4c65c57d91912ccc8
lander247 <electronicmail837@gmail.com>
2025-03-26 01:08:03 -0600

    changed dmenu colors

--- config.h
+++ config.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark, "-nf", col_medium, "-sb", col_dark, "-sf", col_light, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
 
